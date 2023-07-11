046a4675bc2f72eb7731ca583f2e802f3ca1f558
Ronaldo Bianco <48865175+ronaldobianco@users.noreply.github.com>
2023-07-11 17:40:46 -0300

    feat: blocks

--- src/grid.py
+++ src/grid.py
@@ -1,31 +1,18 @@
 import pygame
+from colors import Colors
 class Grid:
     def __init__(self):
         self.num_rows = 20
         self.num_cols = 10
         self.cell_size = 30
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
-        self.colors = self.get_cell_collors()
+        self.colors = Colors.get_cell_colors()
     
     def print_grid(self):
         for row in range(self.num_rows):
             for col in range(self.num_cols):
                 print(self.grid[row][col], end = ' ')
             print()
-    
-    def get_cell_collors(self):
-        dark_grey = (26, 31, 40)
-        green = (47, 230, 23)
-        red = (232, 18, 18)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (166, 0, 247)
-        cyan = (21, 204, 209)
-        blue = (13, 64, 216)
-        white = (255, 255, 255)
-        dark_blue = (44, 44, 127)
-        light_blue = (59, 85, 162)
-        return [dark_grey, green, red, orange, yellow, purple, cyan, blue, white, dark_blue, light_blue]
     
     def draw(self, screen):
         for row in range(self.num_rows):
