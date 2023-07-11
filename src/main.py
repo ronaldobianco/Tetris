be887484b84e3686d1bfa176bc18143f8c06d67e
Ronaldo Bianco <48865175+ronaldobianco@users.noreply.github.com>
2023-07-11 17:13:51 -0300

    feat: grid funcitonality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import pygame
 import sys
 from settings import *
+from grid import Grid
 pygame.init()
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -15,5 +22,6 @@ while True:
             sys.exit()
     #Screen Background
     screen.fill(DARK_BLUE)
+    game_grid.draw(screen)
     pygame.display.update()
     clock.tick(60)
