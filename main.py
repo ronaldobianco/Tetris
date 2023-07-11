c42d9ff6ef183db40bdb81ef1d77c5a58ca60001
Ronaldo Bianco <48865175+ronaldobianco@users.noreply.github.com>
2023-07-11 16:41:00 -0300

    feat:game base settings

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
 import sys
-
+from settings import *
 pygame.init()
 
 screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Screen Background
+    screen.fill(DARK_BLUE)
+    pygame.display.update()
+    clock.tick(60)
